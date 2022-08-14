652ce0a283e014fce834c20406e2b323928feecb
Olexandr Nikolaevsky <olexandr.nikolaevsky@if.no>
2022-08-14 22:42:41 +0300

    Add server call

--- test-static-web-app/src/App.js
+++ test-static-web-app/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/MyTestFunc`)).json();
+      setData(text);
+    })();
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
